1f663246ec94143c979185d0fecfe99b579a67a4
Camille <camillegomes74@gmail.com>
2022-03-22 19:26:05 -0300

    Add 'div' and className

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './App.css'
 
 const App = () =>{
   const message = "Olá, mundo!"
-  return(
-    <h1>{message}</h1>
-  )
+  return <div className='container'>
+    
+  </div>
 }
 
 export default App;
