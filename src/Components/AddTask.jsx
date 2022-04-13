16944dd5d95cb2ac67777a5d894204f9177568cd
Camille <camillegomes74@gmail.com>
2022-04-12 22:38:13 -0300

    Feat: import button

--- src/Components/AddTask.jsx
+++ src/Components/AddTask.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Button from "./Button";
 import './AddTask.css';
 
 const AddTask = () => {
     return ( 
-        <div className=".addTask_container">
+        <div className="addTask_container">
             <input type="text" className="inputTask" />
+            <Button>Add</Button>
         </div>
     );
 }
