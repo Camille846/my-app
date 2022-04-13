48a9af1087a00678294dda3b85d0497edd801e77
Camille <camillegomes74@gmail.com>
2022-04-13 19:29:00 -0300

    Feat: App Title

--- src/Components/AddTask.jsx
+++ src/Components/AddTask.jsx
@@ -5,8 +5,11 @@ import './AddTask.css';
 const AddTask = () => {
     return ( 
         <div className="addTask_container">
+            <h1>Minhas Tarefas</h1>
+            <div className="input_container">
             <input type="text" className="inputTask" />
-            <Button>Add</Button>
+            <Button>Adicionar tarefa</Button>
+            </div>
         </div>
     );
 }
