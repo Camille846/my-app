e212aff0116a7a5e67170a6b244004d0ee4070b4
Camille <camillegomes74@gmail.com>
2022-04-12 22:09:38 -0300

    Create AddTask.jsx

---
+++ src/Components/AddTask.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './AddTask.css';
+
+const AddTask = () => {
+    return ( 
+        <div className=".addTask_container">
+            <input type="text" className="inputTask" />
+        </div>
+    );
+}
+ 
+export default AddTask;
